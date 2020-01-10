57e6c4341ce09b6eb3c62a626c19ea3dea9e39af
ccrisolo <59452934+ccrisolo@users.noreply.github.com>
2020-01-10 05:31:09 -0800

    updated game logic

--- wdi-fundamentals-memorygame/memory-game (1)/memory-game/js/main.js
+++ wdi-fundamentals-memorygame/memory-game (1)/memory-game/js/main.js
@@ -1,7 +1,21 @@
-let cardOne = "King";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "Queen";
+const cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+const cardOne = cards[0];
+const cardTwo = cards[2];
+const cardThree = cards[1];
+const cardFour = cards[3];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+		alert("Sorry, try again.");
+	}
+}
